d33b6363412ce25aa23e55467d806ea1fdfc1eaf
maximecarrion <maxime.carrion@se.com>
2024-03-09 16:13:48 +0100

    Etablissements & Géo Finalisés avec calculs des distances

--- src/fi_functions.py
+++ src/fi_functions.py
@@ -113,3 +113,29 @@ def convert_columns_to_lower(df):
     
     df.columns = [col.lower() for col in df.columns]
     return df
+
+
+#################################################FONCTION CALCUL DISTANCE ENTRE 2 VILLES A PARTIR DES COORDONNEES GPS#########################################
+#variables = (latitude1, longitude1, latitude2, longitude2, unit = 'miles' ou 'km')
+from numpy import sin, cos, arccos, pi, round 
+def rad2deg(radians): 
+    degrees = radians * 180 / pi 
+    return degrees 
+
+def deg2rad(degrees): 
+    radians = degrees * pi / 180 
+    return radians 
+
+def getDistanceBetweenPointsNew(latitude1, longitude1, latitude2, longitude2, unit = 'km'): 
+    theta = longitude1 - longitude2 
+    distance = 60 * 1.1515 * rad2deg( 
+        arccos( 
+            (sin(deg2rad(latitude1)) * sin(deg2rad(latitude2))) + 
+            (cos(deg2rad(latitude1)) * cos(deg2rad(latitude2)) * cos(deg2rad(theta))) 
+        ) 
+    ) 
+    
+    if unit == 'miles': 
+        return round(distance, 2) 
+    if unit == 'km': 
+        return round(distance * 1.609344, 2)
