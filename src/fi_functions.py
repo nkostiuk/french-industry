3c3d68b7b7e0ffdfaefa6c743496aafa6ddc60d5
maximecarrion <maxime.carrion@se.com>
2024-03-10 19:10:02 +0100

    maj fonction

--- src/fi_functions.py
+++ src/fi_functions.py
@@ -150,3 +150,30 @@ def add_leading_zeros(df, column_name, desired_length):
     
     # Add leading zeros to match the desired length
     df[column_name] = df[column_name].str.zfill(desired_length)
+
+
+
+#################################################FONCTION CALCUL DISTANCE ENTRE 2 VILLES A PARTIR DES COORDONNEES GPS#########################################
+#variables = (latitude1, longitude1, latitude2, longitude2, unit = 'miles' ou 'km')
+from numpy import sin, cos, arccos, pi, round 
+def rad2deg(radians): 
+    degrees = radians * 180 / pi 
+    return degrees 
+
+def deg2rad(degrees): 
+    radians = degrees * pi / 180 
+    return radians 
+
+def getDistanceBetweenPointsNew(latitude1, longitude1, latitude2, longitude2, unit = 'km'): 
+    theta = longitude1 - longitude2 
+    distance = 60 * 1.1515 * rad2deg( 
+        arccos( 
+            (sin(deg2rad(latitude1)) * sin(deg2rad(latitude2))) + 
+            (cos(deg2rad(latitude1)) * cos(deg2rad(latitude2)) * cos(deg2rad(theta))) 
+        ) 
+    ) 
+    
+    if unit == 'miles': 
+        return round(distance, 2) 
+    if unit == 'km': 
+        return round(distance * 1.609344, 2)
